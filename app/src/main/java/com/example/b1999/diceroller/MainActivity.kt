f38ecec9f114e04c8b0b1ed072a780888305e46a
BrandonAGoodwin <35398295+BrandonAGoodwin@users.noreply.github.com>
2019-01-04 18:08:59 +0000

    Completed app using text for the roll. Next: Use images to represent the roll outcomes.

--- app/src/main/java/com/example/b1999/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/b1999/diceroller/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.b1999.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll!"
+        rollButton.setOnClickListener {
+            //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        var randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
