96012514476eea65f7e83d7f4643b45b104b0319
BrandonAGoodwin <35398295+BrandonAGoodwin@users.noreply.github.com>
2019-01-04 18:21:44 +0000

    Finished working app with dice images instead of text. Next: Applying good practices to maximise the efficiency of the program.

--- app/src/main/java/com/example/b1999/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/b1999/diceroller/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.b1999.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +14,22 @@ class MainActivity : AppCompatActivity() {
         var rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll!"
         rollButton.setOnClickListener {
-            //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        var randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
